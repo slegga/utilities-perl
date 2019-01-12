0bf758817b5ff6cb6e2ce9bbdcb61a4dd3c3cef0
Stein Hammer <steihamm@gmail.com>
2019-01-12 12:26:47 +0100

    Add pod

--- lib/SH/UseLib.pm
+++ lib/SH/UseLib.pm
@@ -2,6 +2,8 @@ package SH::UseLib;
 use FindBin;
 use Mojo::File 'path';
 
+
+
 sub import {
     my @tmp = @{path($INC{'SH/UseLib.pm'})->to_abs->to_array};
 #    warn join '§',@tmp;
@@ -18,3 +20,28 @@ sub import {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+SH::UseLib - Find all the lib catalog and put it in @INC
+
+=head1 SYNOPSIS
+
+ use lib '../../utilities-perl/lib';
+ use SH::UseLib;
+
+=head1 DESCRIPTION
+
+Find all the lib catalog and put it in @INC
+
+=head1 FUNTIONS
+
+=head2 import
+
+Called automatically with use SH::UseLib;
+
+Put all the lib catalogs in @INC;
+
+=cut
