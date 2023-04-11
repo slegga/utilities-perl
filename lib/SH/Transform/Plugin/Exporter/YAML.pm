edda20a463c6402741ec805a889d1831ee5c1018
Stein Hammer <steihamm@gmail.com>
2023-04-11 21:08:32 +0200

    update pod

--- lib/SH/Transform/Plugin/Exporter/YAML.pm
+++ lib/SH/Transform/Plugin/Exporter/YAML.pm
@@ -3,6 +3,29 @@ use Mojo::Base 'SH::Transform::Exporter',-signatures;
 use Mojo::File 'path';
 use YAML::Syck;
 
+=head1 NAME
+
+SH::Transform::Plugin::Exporter::YAML - create file and fill it with data in yaml format
+
+=head1 SYNOPSIS
+
+    use SH::Transform;
+    my $trans = SH::Transform->new(Exporter=>SH::Transform::Plugin::Exporter::YAML->new);
+    $trans->transform({file=>'test.json'},{file=>'test.yaml'});
+
+=head1 DESCRIPTION
+
+Enable export to yaml formated file.
+
+=head1 METHODS
+
+=head2 is_accepted
+
+...
+
+=cut
+
+
 sub is_accepted($self, $args) {
 
     my $extname;
